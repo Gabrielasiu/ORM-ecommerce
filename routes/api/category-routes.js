db15eeab1999dd380b7b590c441014b99b342524
gabriela <gabysiiu@hotmail.com>
2024-07-20 17:21:44 -0700

    get routes added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,15 +3,14 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/api/categories', async (req, res) => {
-
-  // find all categories 
+router.get('/', async (req, res) => {
+  // find all categories
   // be sure to include its associated Products
   try {
-    const categoriesData = await Category.findAll({
+    const categoryData= await Category.findAll({
       include: [{ model: Product }],
     });
-    res.status(200).json(categoriesData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -21,16 +20,16 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoriesData = await Category.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
 
-    if (!categoriesData) {
-      res.status(404).json({ message: 'No product found with that id!' });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
 
-    res.status(200).json(categoriesData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
